69ee2be053379da97101327473c2134e6689e839
Evgeniy Morskoy <zhekamorskoi@gmail.com>
2018-08-06 04:28:30 +0300

    api added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ResumeAPI from './api';
+
 import Header from './Components/Header';
 import About from './Components/About';
 import Resume from './Components/Resume';
@@ -7,6 +9,9 @@ import CallToAction from './Components/CallToAction';
 import Testimonials from './Components/Testimonials';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
+
+const allData = ResumeAPI.all();
+console.log(allData);
 
 class App extends Component {
   render() {
